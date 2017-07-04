3b176141334c5f648a008ff9982de82d1abfcb7f
tsareva <atana@bastilia.ru>
2017-07-04 14:16:51 +0300

    Add verification for vkontakte lib installation

--- members_compare.py
+++ members_compare.py
@@ -6,8 +6,21 @@ sys.setdefaultencoding('utf-8')
 sys.path.append('files/')
 from group import *
 from create_token import *
-import time, vkontakte
+import time
 
+#check for vkontakte lib
+from pip import main, get_installed_distributions
+installed_packages = get_installed_distributions()
+flat_installed_packages = [package.project_name for package in installed_packages]
+if 'vkontakte' in flat_installed_packages: 
+	pass
+else: 
+	print "Vkontakte module is needed. Trying to install..."
+	main(["install", 'vkontakte'])
+
+import vkontakte
+
+#Check if you have token
 try:
 	get_server_time()
 except vkontakte.api.VKError as error:
@@ -33,3 +46,5 @@ intersection = len(set(members1).intersection(members2))
 print "There are %s common users" % intersection
 print "It is %s percent for group with %s id" % ((intersection*100/count1), id1)
 print "It is %s percent for group with %s id" % ((intersection*100/count2), id2)
+
+raw_input("Press enter to quit")
