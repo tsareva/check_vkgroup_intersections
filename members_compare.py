709f1dcb5d1006ac53f5d603cd50cb8070b508f4
tsareva <atana@bastilia.ru>
2018-01-18 15:40:23 +0300

    improve work with id like 'club7777'

--- members_compare.py
+++ members_compare.py
@@ -1,6 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+#VK API can't correctly work with short names like "club7777"
+#So we need to check for it and correct if it is needed
+def get_group_id(vk_club):
+    if vk_club.find('club') != -1:
+        group_id = vk_club[4:]
+    elif vk_club.find('event') != -1:
+        group_id = vk_club[5:]
+    elif vk_club.find('public') != -1:
+        group_id = vk_club[6:]
+    else:
+        group_id = vk_club
+    try:
+        int(group_id)
+    except: 
+        group_id = vk_club
+    return group_id
 
 #check for vk_api lib
 from pip import main, get_installed_distributions
@@ -32,8 +48,8 @@ vk_session.auth()
 tools = vk_api.VkTools(vk_session)
 vk = vk_session.get_api()
 
-id1 = input('Enter first group id: ')
-id2 = input('Enter second group id: ')
+id1 = get_group_id(input('Enter first group id: '))
+id2 = get_group_id(input('Enter second group id: '))
 
 members1 = tools.get_all('groups.getMembers', 1000, {'group_id': id1})["items"]
 members2 = tools.get_all('groups.getMembers', 1000, {'group_id': id2})["items"]
@@ -41,7 +57,9 @@ members2 = tools.get_all('groups.getMembers', 1000, {'group_id': id2})["items"]
 intersection = len(set(members1).intersection(members2))
 
 print("There are %s common users" % intersection)
-print("It is %s percent for group with %s id" % (round(intersection*100/len(members1), 1), id1))
-print("It is %s percent for group with %s id" % (round(intersection*100/len(members2), 1), id2))
 
+if intersection > 0:
+	print("It is %s percent for group with %s id" % (round(intersection*100/len(members1), 1), id1))
+	print("It is %s percent for group with %s id" % (round(intersection*100/len(members2), 1), id2))
+	
 input("Press enter to quit")
